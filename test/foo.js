b185648451b4df43c90109e8065bcf86e51f8e8d
Yalin Gunayer <yalin.gunayer@emlakjet.com>
2017-11-03 16:41:49 +0300

    Initial commit

---
+++ test/foo.js
@@ -0,0 +1,8 @@
+const mocha = require('mocha');
+const expect = require('chai').expect;
+
+describe('foo', () => {
+    it('1 == 1 should be true', () => {
+        expect(1).to.equal(1);
+    })
+})
